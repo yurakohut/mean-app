efbb20d04d1a9817ba6c3e2a669ea8569a392ac5
yurakohut <yurakohut97@gmail.com>
2020-04-21 16:53:25 +0300

    added the ability to edit the post

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -16,9 +16,10 @@ export class PostsService {
 
   getPosts() {
     // return [...this.posts];
-    this.http.get<{ message: string, posts: any }>(
-      'http://localhost:3000/api/posts'
-    )
+    this.http
+      .get<{ message: string, posts: any }>(
+        'http://localhost:3000/api/posts'
+      )
       .pipe(map((postData) => {
         return postData.posts.map(post => {
           return {
@@ -38,9 +39,14 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   };
 
+  getPost(id: string) {
+    return this.http.get<{ _id: string, title: string, content: string }>('http://localhost:3000/api/posts/' + id);
+  };
+
   addPost(title: string, content: string) {
     const post: IPost = { id: null, title, content };
-    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
+    this.http
+      .post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         const id = responseData.postId;
         post.id = id;
@@ -49,8 +55,22 @@ export class PostsService {
       });
   };
 
+  updatePost(id: string, title: string, content: string) {
+    const post: IPost = { id, title, content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  };
+
   deletePost(postId: string) {
-    this.http.delete('http://localhost:3000/api/posts/' + postId)
+    this.http
+      .delete('http://localhost:3000/api/posts/' + postId)
       .subscribe(() => {
         const updatedPosts = this.posts.filter(post => post.id !== postId)
         this.posts = updatedPosts;
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PostsService } from './../../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { IPost } from 'src/app/interfaces/post.interface';
 
 @Component({
   selector: 'app-post-create',
@@ -10,17 +12,40 @@ import { PostsService } from './../../posts.service';
 export class PostCreateComponent implements OnInit {
   enteredTitle: string = '';
   enteredContent: string = '';
-
-  constructor(public postsService: PostsService) { }
+  private mode = 'create';
+  private postId: string;
+  post: IPost;
+  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          }
+        });
+      }
+      else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    })
   }
 
-  onAddPost(form: NgForm): void {
+  onSavePost(form: NgForm): void {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content)
+    }
     form.resetForm();
   };
 }
